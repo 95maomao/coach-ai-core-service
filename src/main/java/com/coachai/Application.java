ac17e403c397cdd786bc9a0dd962e618a0042c55
lijinzhang.ljz <lijinzhang.ljz@lazada.com>
2025-09-18 21:01:25 +0800

    init

---
+++ src/main/java/com/coachai/Application.java
@@ -0,0 +1,15 @@
+package com.coachai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Coach AI Core Service Application
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
