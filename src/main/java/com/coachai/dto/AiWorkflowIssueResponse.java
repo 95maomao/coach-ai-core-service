23392afba87be0c48206e8850153a7d665f1139c
Tina-xiao <1597872231@qq.com>
2025-09-21 14:15:25 +0800

    修改反序列化问题

--- src/main/java/com/coachai/dto/AiWorkflowIssueResponse.java
+++ src/main/java/com/coachai/dto/AiWorkflowIssueResponse.java
@@ -116,14 +116,14 @@ public class AiWorkflowIssueResponse {
     @AllArgsConstructor
     @Builder
     public static class StructData {
-        // 标准运动姿势参考
-        private String poseReference;
+        // 标准运动姿势参考（数组形式）
+        private List<String> poseReference;
 
         // 症状描述
         private DiagnosisData message;
 
-        // 康复视频
-        private String rehabilitationVideos;
+        // 康复视频（数组形式）
+        private List<String> rehabilitationVideos;
     }
 
 
